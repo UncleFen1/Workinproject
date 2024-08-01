2e3bb1ab285e163e62e8b34ce850499d0bf60476
freecdog <cdog@ya.ru>
2024-08-01 11:59:15 +0200

    Revert "feat: unity version upgrade"
    This reverts commit 18925a1c8d6e464e524b3e9cd24358cf3ce6a58e.

--- BuildScripts/windows/web.cmd
+++ BuildScripts/windows/web.cmd
@@ -1,1 +1,1 @@
-"C:\unity\editor\2022.3.40f1\Editor\Unity.exe" -quit -batchmode -nographics -buildTarget webgl -executeMethod BuildScript.BuildForWeb -projectPath ..\..
+"C:\unity\editor\2022.3.13f1\Editor\Unity.exe" -quit -batchmode -nographics -buildTarget webgl -executeMethod BuildScript.BuildForWeb -projectPath ..\..
