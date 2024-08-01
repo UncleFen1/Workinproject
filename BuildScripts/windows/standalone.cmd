18925a1c8d6e464e524b3e9cd24358cf3ce6a58e
freecdog <cdog@ya.ru>
2024-08-01 10:13:25 +0200

    feat: unity version upgrade

--- BuildScripts/windows/standalone.cmd
+++ BuildScripts/windows/standalone.cmd
@@ -1,1 +1,1 @@
-"C:\unity\editor\2022.3.13f1\Editor\Unity.exe" -quit -batchmode -nographics -buildTarget win64 -executeMethod BuildScript.BuildForWindows64 -projectPath ..\..
+"C:\unity\editor\2022.3.40f1\Editor\Unity.exe" -quit -batchmode -nographics -buildTarget win64 -executeMethod BuildScript.BuildForWindows64 -projectPath ..\..
