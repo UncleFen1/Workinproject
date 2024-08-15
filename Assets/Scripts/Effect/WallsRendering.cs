0c1160cfef486978ac184f53f111d2b5fb19d158
Андрей Лопатюк <lopatyuk.andrey@gmail.com>
2024-08-16 00:02:26 +0300

    wip
    1. death screen comics added
    2. fixed hands rotation and mirroring for different direcctions
    3. player walking animation added and it works for different directions
    4. WallsRendering script WIP

---
+++ Assets/Scripts/Effect/WallsRendering.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WallsRendering : MonoBehaviour
+{
+    public TilemapRenderer[] enableTilemaps; // Массив тайлмап рендереров для включения
+    public TilemapRenderer[] disableTilemaps; // Массив тайлмап рендердеров для отключения
+
+    void OnTriggerEnter2D(EdgeCollider2D other)
+    {
+        // Проверяем, что объект, с которым мы сталкиваемся, является нужным объектом
+        if (other.CompareTag("Player")) // Измените "Player" на нужный вам тег
+        {
+            ToggleTilemaps(); // Переключаем состояние тайлмапов
+        }
+    }
+
+    void ToggleTilemaps()
+    {
+        // Включаем или отключаем тайлмапы в зависимости от их текущего состояния
+        foreach (TilemapRenderer tilemapRenderer in enableTilemaps)
+        {
+            tilemapRenderer.enabled = !tilemapRenderer.enabled; // Переключаем видимость тайлмапа
+        }
+
+        foreach (TilemapRenderer tilemapRenderer in disableTilemaps)
+        {
+            tilemapRenderer.enabled = !tilemapRenderer.enabled; // Переключаем видимость тайлмапа
+        }
+    }
+}
+
