ea0cab2ae7a683e6d29756af4e6bcc802c779c1c
Андрей Лопатюк <lopatyuk.andrey@gmail.com>
2024-08-20 22:05:32 +0300

    misc
    -tutorial lvl started
    -ui completed

---
+++ Assets/Scripts/UI/BackgroundParallax.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+
+    //script working just for main menu background, so i hope u will not curse me
+{
+    public float parallaxFactor;
+    private RectTransform rectTransform;
+    private Vector3 previousMousePosition;
+
+    public float minX;
+    public float maxX;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        previousMousePosition = Input.mousePosition;
+    }
+
+    private void Update()
+    {
+        Vector3 currentMousePosition = Input.mousePosition;
+
+        float deltaX = currentMousePosition.x - previousMousePosition.x;
+
+        float newX = rectTransform.anchoredPosition.x + deltaX * parallaxFactor;
+
+        newX = Mathf.Clamp(newX, minX, maxX);
+
+        rectTransform.anchoredPosition = new Vector2(newX, rectTransform.anchoredPosition.y);
+
+        previousMousePosition = currentMousePosition;
+    }
+}
